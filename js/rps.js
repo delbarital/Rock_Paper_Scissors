84f4da908ade06d12491f99052960afe4f090462
delbarital <delbarital@gmail.com>
2014-11-16 03:23:44 +0200

    add the api for rps.js

---
+++ js/rps.js
@@ -0,0 +1,39 @@
+/*
+hides the red arrows
+■ changes the red text to “GO!”
+■ resets any additional variables
+*/
+function newRPS() {
+
+}
+
+/*
+ returns “rock”, “paper” or “scissors” randomly
+
+ */
+function player2RPS() {
+ 
+}
+
+/*
+ assuming player1, and player2 are valid RPS options (“rock”, “paper” or
+“scissors”), the function returns: 0 for a tie, 1 if player1 won, and 2 if
+player2 won
+ */
+function checkRPS(player1, player2) {
+ 
+ }
+
+/*
+
+assuming player1 is a valid RPS option (“rock”, “paper” or “scissors”)
+■ marks player2 action by making one of the upper red arrows visible
+■ get player2 action using player2RPS()
+■ marks player2 action by making one of the lower red arrows visible
+■ call checkRPS(player1, player2) to find the game result
+■ changes the red text to one of:
+● “Player1 won!”
+● “Player2 won!”
+● “Tie!”
+*/
+function playRPS(player1) {...}
